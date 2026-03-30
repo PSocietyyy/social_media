b9c8248aad1495381e9af66b799c6c0d4d9d2c85
punishcore <arkadanisaysstuff@gmail.com>
2026-03-31 01:01:17 +0700

    refactor: replace old backend with fresh nestjs setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,28 +1,8 @@
-import 'dotenv/config';
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ResponseInterceptor } from './common/interceptors/response/response.interceptor';
-import { HttpExceptionFilter } from './common/filters//http-exception/http-exception.filter';
-import { AllExceptionFilter } from './common/filters/all-exception/all-exception.filter';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  console.log('DB URL:', process.env.DATABASE_URL);
-
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalInterceptors(new ResponseInterceptor());
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, 
-      forbidNonWhitelisted: true, 
-    }),
-  );
-  
-  app.useGlobalFilters(new HttpExceptionFilter(), new AllExceptionFilter());
-
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
